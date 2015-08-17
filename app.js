14db010b8efe2007d1625f92ac63f63f8a09e1d2
Graham Mueller <graham@branchmetrics.io>
2015-08-17 11:25:30 -0500

    Change how port is determined

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var http = require('http');
 var config = require('./config');
+var port = process.env.PORT || config.server.port || 3000;
 
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 });
 
-server.listen(config.server.port, function(e) {
+server.listen(port, function(e) {
     if (e) {
         console.log('Failed to start server:', e);
         process.exit(1);
     }
 
-    console.log('Server running on port ' + config.server.port);
+    console.log('Server running on port ' + port);
 });
